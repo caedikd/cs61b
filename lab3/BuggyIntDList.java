801a336d0b40eb0731b0e1c5c5efd0bafd04e5dd
Caedi Seim <caediseim@berkeley.edu>
2021-09-09 22:47:39 -0700

    Completed lab3

--- lab3/BuggyIntDList.java
+++ lab3/BuggyIntDList.java
@@ -66,20 +66,22 @@ public class BuggyIntDList extends IntDList {
         DNode temp = null;
         DNode p = _front;
 
+
         // HINT: What does this while loop do? Use Debugger and Java Visualizer to figure out.
         while (p != null) {
             temp = p._prev;
             p._prev = p._next;
             p._next = temp;
-            p = p._next;        // FIXME: Replace this line (if needed). HINT: Use debugger and Java Visualizer to figure out what it does.
+            p = p._prev;        // FIXME: Replace this line (if needed). HINT: Use debugger and Java Visualizer to figure out what it does.
         }
 
         // HINT: What does this if block do? Use Debugger and Java Visualizer to figure out.
         if (temp != null) {
             // ------ WRITE ADDITIONAL CODE HERE AND ONLY HERE (IF NEEDED) -----
-
+            _back = _front;
             // -----------------------------------------------------------------
-            _front = temp._next;    // FIXME: Replace this line (if needed). HINT: Use debugger and Java Visualizer to figure out what it does.
+            _front = temp._prev;    // FIXME: Replace this line (if needed). HINT: Use debugger and Java Visualizer to figure out what it does.
         }
+
     }
 }
